476bbf310587c043c725f98341b7ca2e20677b3e
badou0410 <72540344+badou0410@users.noreply.github.com>
2021-01-04 17:28:29 +0800

    Initial commit

---
+++ android_num_28_notifications/app/src/main/java/com/example/android_num_28_notifications/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android_num_28_notifications;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click_btn(View v)
+    {
+        NotificationManager msg_str = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        Bitmap msg_img = BitmapFactory.decodeResource(getResources(), R.mipmap.zat);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ico)                       //Show a small icon in the upper status bar
+                .setLargeIcon(msg_img)                            //Icon
+                .setContentTitle("Notification Test")             //Title
+                .setContentText("#$%^$#$%^&*^%$%^&")              //Text
+                .setWhen(System.currentTimeMillis())
+                .setDefaults(Notification.DEFAULT_ALL)            //Vibration reminder
+                .setLights(Color.GREEN, 1000, 1000) //Flash
+                .setContentIntent(pendingIntent)                  //Set Intent
+                .setAutoCancel(true)                              //Click close
+                .build();
+
+        msg_str.notify(0, notification);
+    }
+}
